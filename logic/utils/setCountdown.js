24072a3432d22d28786280922e03a026fc983ae9
Tarik <tarikmerrylees@gmail.com>
2023-10-01 15:06:58 +0100

    create function to set time between instructions

---
+++ logic/utils/setCountdown.js
@@ -0,0 +1,5 @@
+function setCountdown(timeToManeuver) {
+  // create a random number of seconds between 30 and 90
+  const randomSeconds = Math.floor(Math.random() * 60) + 30
+  return timeToManeuver + randomSeconds
+}
