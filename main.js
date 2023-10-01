989301d692f5d6fef1ec7bb4bef10647afbb6af1
Tarik <tarikmerrylees@gmail.com>
2023-10-01 14:54:13 +0100

    change directory structure

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 // Generators
-const getRandomHeading = require('./utils/generators/getRandomHeading.js');
-const getRandomAltitude = require('./utils/generators/getRandomAltitude.js');
-const getRandomSpeed = require('./utils/generators/getRandomSpeed.js');
+const getRandomHeading = require('./logic/utils/generators/getRandomHeading.js');
+const getRandomAltitude = require('./logic/utils/generators/getRandomAltitude.js');
+const getRandomSpeed = require('./logic/utils/generators/getRandomSpeed.js');
 
 // Calculators
-const turnCalculator = require('./utils/calculators/turnCalculator.js');
-const climbDescentCalculator = require('./utils/calculators/climbDescentCalculator.js');
-const speedCalculator = require('./utils/calculators/speedCalculator.js');
+const turnCalculator = require('./logic/utils/calculators/turnCalculator.js');
+const climbDescentCalculator = require('./logic/utils/calculators/climbDescentCalculator.js');
+const speedCalculator = require('./logic/utils/calculators/speedCalculator.js');
 
 
 console.log(getRandomSpeed(80));
