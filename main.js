26c6bffc22a16dbd4d2f1f1b076d30d8b1794b08
Tarik <tarikmerrylees@gmail.com>
2023-10-01 14:52:48 +0100

    change directory structure

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ const getRandomAltitude = require('./utils/generators/getRandomAltitude.js');
 const getRandomSpeed = require('./utils/generators/getRandomSpeed.js');
 
 // Calculators
-const turnCalculator = require('./utils/turnCalculator.js');
-const climbDescentCalculator = require('./utils/climbDescentCalculator.js');
+const turnCalculator = require('./utils/calculators/turnCalculator.js');
+const climbDescentCalculator = require('./utils/calculators/climbDescentCalculator.js');
+const speedCalculator = require('./utils/calculators/speedCalculator.js');
 
 
 console.log(getRandomSpeed(80));
