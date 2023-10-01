c58d99ef26dc174f19bc7d852007e734f2b116ca
Tarik <tarikmerrylees@gmail.com>
2023-10-01 18:38:29 +0100

    refactor main

--- main.js
+++ main.js
@@ -15,31 +15,33 @@ import {updateUI} from './logic/updateUI.js';
 // countdown
 import {setCountdown} from './logic/utils/setCountdown.js';
 
+
+const triggerInstruction = (doc, aircraft) => {
+  const instructionData = generateInstruction(aircraft)
+  if (instructionData.instructionType === 'heading') aircraft.setHeading(instructionData.heading);
+  if (instructionData.instructionType === 'altitude') aircraft.setAltitude(instructionData.altitude);
+  if (instructionData.instructionType === 'speed') aircraft.setSpeed(instructionData.speed);
+
+  updateUI(doc, aircraft, instructionData.instruction)
+    
+  // set timer
+  let countDown
+  if (instructionData.instructionType === 'heading') countDown = setCountdown(instructionData.turnCalculation.turnTime);
+  if (instructionData.instructionType === 'altitude') countDown = setCountdown(instructionData.climbDescentCalculation.timeToAlt);
+  if (instructionData.instructionType === 'speed') countDown = setCountdown(instructionData.speedCalculation.accelerationTime);
+}
+
 ((d) => {
   // create an aircraft
-  const aircraft = new Aircraft();
+  const currentAircraft = new Aircraft();
 
   // set initial aircraft values
-  d.getElementById('targetHeading').innerText = formatHeading(aircraft.heading);
-  d.getElementById('targetAltitude').innerText = formatAltitude(aircraft.altitude);
-  d.getElementById('targetAirspeed').innerText = formatAirspeed(aircraft.speed);
-
-  d.getElementById('generateInstruction').addEventListener('click', () => {
-    const instructionData = generateInstruction(aircraft)
-    console.log(instructionData)
-    if (instructionData.instructionType === 'heading') aircraft.setHeading(instructionData.heading);
-    if (instructionData.instructionType === 'altitude') aircraft.setAltitude(instructionData.altitude);
-    if (instructionData.instructionType === 'speed') aircraft.setSpeed(instructionData.speed);
-
-    updateUI(d, aircraft, instructionData.instruction)
-    
-    // set timer
-    let countDown
-    if (instructionData.instructionType === 'heading') countDown = setCountdown(instructionData.turnCalculation.turnTime);
-    if (instructionData.instructionType === 'altitude') countDown = setCountdown(instructionData.climbDescentCalculation.timeToAlt);
-    if (instructionData.instructionType === 'speed') countDown = setCountdown(instructionData.speedCalculation.accelerationTime);
-    console.log(countDown)
+  d.getElementById('targetHeading').innerText = formatHeading(currentAircraft.heading);
+  d.getElementById('targetAltitude').innerText = formatAltitude(currentAircraft.altitude);
+  d.getElementById('targetAirspeed').innerText = formatAirspeed(currentAircraft.speed);
 
 
-  })
+
+
+  d.getElementById('generateInstruction').addEventListener('click', () => triggerInstruction(d,currentAircraft))
 })(document)
