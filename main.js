fdde5d51ff24e6067e73d3b83997b72b509ab50a
Tarik <tarikmerrylees@gmail.com>
2023-10-01 14:49:32 +0100

    create random speed generator

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const turnCalculator = require('./utils/turnCalculator.js');
 const getRandomHeading = require('./utils/getRandomHeading.js');
 const climbDescentCalculator = require('./utils/climbDescentCalculator.js');
-const getRandomAltitude = require('./utils/getRandomAltitude.js');
+const getRandomAltitude = require('./utils/generators/getRandomAltitude.js');
 const getRandomSpeed = require('./utils/getRandomSpeed.js');
 
 
