b81f733a85e96755e6897075a48de51d6ef57835
Tarik <tarikmerrylees@gmail.com>
2023-10-03 17:20:23 +0100

    correct pronounciation of numbers

--- logic/utils/textToSpeech/numberToPhoneitc.js
+++ logic/utils/textToSpeech/numberToPhoneitc.js
@@ -2,15 +2,15 @@
 export function numberToPhonetic(number) {
   const digitMap = {
     '0': 'zero',
-    '1': 'one',
+    '1': 'won',
     '2': 'two',
-    '3': 'three',
-    '4': 'four',
-    '5': 'five',
+    '3': 'tree',
+    '4': 'fower',
+    '5': 'fife',
     '6': 'six',
-    '7': 'seven',
+    '7': 'sefen',
     '8': 'eight',
-    '9': 'nine',
+    '9': 'niner',
     '°': 'degrees'
   };
   return number.split('').map(digit => digitMap[digit] || digit).join(' ');
