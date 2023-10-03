8fd4835d5a0323b841c6949457c9619e1fe4afb9
Tarik <tarikmerrylees@gmail.com>
2023-10-03 17:13:42 +0100

    fix wrong voice language bug on ios 4

--- logic/utils/textToSpeech/textToSpeech.js
+++ logic/utils/textToSpeech/textToSpeech.js
@@ -1,44 +1,34 @@
 export function textToSpeech(textToSpeak) {
   if ('speechSynthesis' in window) {
     const utterance = new SpeechSynthesisUtterance(textToSpeak);
+    utterance.lang = 'en-US';
+    utterance.rate = 1.0; // Speech rate (optional)
+    utterance.pitch = 1.0; // Speech pitch (optional)
 
-    // Function to check if an available voice has a given language code
-    function hasLanguage(voice, language) {
-      return voice.lang.startsWith(language);
+    // Function to check if the current device is iOS
+    function isIOS() {
+      return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
     }
 
-    // Wait for voices to be loaded
-    speechSynthesis.onvoiceschanged = () => {
-      const voices = speechSynthesis.getVoices();
+    // Check if the device is iOS and manually set the voice
+    if (isIOS()) {
+      // Manually set an English voice known to exist on iOS
+      utterance.voice = speechSynthesis.getVoices().find(voice => voice.name === 'Samantha (Enhanced)');
+    }
 
-      // Find the first voice with English language
-      const englishVoice = voices.find((voice) => hasLanguage(voice, 'en'));
-      console.log(voices);
-      
-      if (englishVoice) {
-        utterance.voice = englishVoice;
-        utterance.lang = englishVoice.lang;
-      } else {
-        console.warn('No English voice found. Using the default voice.');
-      }
-
-      utterance.rate = 1.0; // Speech rate (optional)
-      utterance.pitch = 1.0; // Speech pitch (optional)
-
-      utterance.onstart = () => {
-        console.log('Speech started.');
-      };
-
-      utterance.onend = () => {
-        console.log('Speech ended.');
-      };
-
-      utterance.onerror = (event) => {
-        console.error('Speech error:', event.error);
-      };
-
-      speechSynthesis.speak(utterance);
+    utterance.onstart = () => {
+      console.log('Speech started.');
     };
+
+    utterance.onend = () => {
+      console.log('Speech ended.');
+    };
+
+    utterance.onerror = (event) => {
+      console.error('Speech error:', event.error);
+    };
+
+    speechSynthesis.speak(utterance);
   } else {
     console.error('Web Speech API is not supported in this browser.');
   }
