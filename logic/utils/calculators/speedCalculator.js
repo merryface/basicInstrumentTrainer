78b19e2146f41eef6d63608b7bc76266daa69c8d
Tarik <tarikmerrylees@gmail.com>
2023-10-03 13:07:33 +0100

    change timing for speeds

--- logic/utils/calculators/speedCalculator.js
+++ logic/utils/calculators/speedCalculator.js
@@ -1,6 +1,6 @@
 export function speedCalculator(currentSpeed, targetSpeed) {
   // assume 1kt acceleration/deceleration per second
   return {
-    accelerationTime: Math.abs(currentSpeed - targetSpeed)
+    accelerationTime: Math.abs(currentSpeed - targetSpeed)*2.5
   };
 }
