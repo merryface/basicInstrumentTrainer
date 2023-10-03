906e787b86cd010ab886a800fbf6a922553c8613
Tarik <tarikmerrylees@gmail.com>
2023-10-03 10:24:57 +0100

    refactor to move wordToPhonetic in different file

--- logic/utils/textToSpeech/convertInstructionToSpeech.js
+++ logic/utils/textToSpeech/convertInstructionToSpeech.js
@@ -1,48 +1,8 @@
+import { wordToPhonetic } from "./wordToPhonetic.js";
+
 export function convertInstructionToSpeech(instruction) {
   // Split the instruction string into words
   const words = instruction.split(/\s+/);
-
-  // Define a mapping of letters to phonetic representations
-  const phoneticMap = {
-    A: 'alpha',
-    B: 'bravo',
-    C: 'charlie',
-    D: 'delta',
-    E: 'echo',
-    F: 'foxtrot',
-    G: 'golf',
-    H: 'hotel',
-    I: 'india',
-    J: 'juliet',
-    K: 'kilo',
-    L: 'lima',
-    M: 'mike',
-    N: 'november',
-    O: 'oscar',
-    P: 'papa',
-    Q: 'quebec',
-    R: 'romeo',
-    S: 'sierra',
-    T: 'tango',
-    U: 'uniform',
-    V: 'victor',
-    W: 'whiskey',
-    X: 'x-ray',
-    Y: 'yankee',
-    Z: 'zulu',
-    // Add more mappings for other letters if needed
-  };
-
-  // Function to convert a single word into phonetic representation
-  function wordToPhonetic(word) {
-    const phoneticWord = [];
-    for (const letter of word) {
-      const uppercaseLetter = letter.toUpperCase();
-      const phoneticRepresentation = phoneticMap[uppercaseLetter] || uppercaseLetter;
-      phoneticWord.push(phoneticRepresentation);
-    }
-    return phoneticWord.join(' ');
-  }
 
   // Convert only the first word in the instruction to its phonetic representation
   if (words.length > 0) {
