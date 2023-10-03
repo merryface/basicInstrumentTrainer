a63f7931425791ec6dc530b46400c20a2ab1a246
Tarik <tarikmerrylees@gmail.com>
2023-10-03 10:28:33 +0100

    refactor layout of file

--- logic/utils/textToSpeech/convertInstructionToSpeech.js
+++ logic/utils/textToSpeech/convertInstructionToSpeech.js
@@ -10,22 +10,22 @@ export function convertInstructionToSpeech(instruction) {
     words[0] = wordToPhonetic(words[0]);
   }
 
-// Check if the second word is "speed"
-if (words.length > 1 && (words[1] === "speed")) {
-  const number = words[2];
-  const phoneticNumber = numberToPhonetic(number);
-  words[2] = phoneticNumber;
-}
+  // Check if the second word is "speed"
+  if (words.length > 1 && (words[1] === "speed")) {
+    const number = words[2];
+    const phoneticNumber = numberToPhonetic(number);
+    words[2] = phoneticNumber;
+  }
 
-// Check if the second word is or "turn"
-if (words.length > 1 && (words[1] === "turn")) {
-  const number = words[4];
-  const phoneticNumber = numberToPhonetic(number);
-  words[4] = phoneticNumber;
-}
+  // Check if the second word is or "turn"
+  if (words.length > 1 && (words[1] === "turn")) {
+    const number = words[4];
+    const phoneticNumber = numberToPhonetic(number);
+    words[4] = phoneticNumber;
+  }
 
-// Join the words back together to form the modified instruction string
-const phoneticInstruction = words.join(' ');
+  // Join the words back together to form the modified instruction string
+  const phoneticInstruction = words.join(' ');
 
-return phoneticInstruction;
+  return phoneticInstruction;
 }
