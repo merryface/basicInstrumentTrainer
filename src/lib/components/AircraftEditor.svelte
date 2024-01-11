cd400b77cc5670eb220dc75a1ddafa63de82028b
Tarik <tarikmerrylees@gmail.com>
2024-01-11 23:43:07 +0000

    style aircraft settings

--- src/lib/components/AircraftEditor.svelte
+++ src/lib/components/AircraftEditor.svelte
@@ -4,35 +4,62 @@
 
 </script>
 
-<h3>Aircraft Editor</h3>
 
-<form>
-  <label>
-    Callsign:
-    <input type="text" bind:value={$currentAircraft.callsign}>
-  </label>
+<div class="container border-solid border-2 border-sky-500 p-3 rounded-md">
 
-  <label>
-    Max Altitude:
-    {formatAltitude($currentAircraft.maxAltitude)}
-    <input type="range" min={$currentAircraft.minAltitude + 1000} max="15000" step="500" class="range range-accent" bind:value={$currentAircraft.maxAltitude} />
-  </label>
-
-  <label>
-    Min Altitude:
-    {formatAltitude($currentAircraft.minAltitude)}
-    <input type="range" min="25000" max={$currentAircraft.maxAltitude - 1000} step="100" class="range range-primary" bind:value={$currentAircraft.minAltitude} />
-  </label>
-
-  <label>
-    Max Speed:
-    {$currentAircraft.maxSpeed}kt
-    <input type="range" min={$currentAircraft.minSpeed + 20} max="250" step="5" class="range range-accent" bind:value={$currentAircraft.maxSpeed} />
-  </label>
-
-  <label>
-    Min Speed:
-    {$currentAircraft.minSpeed}kt
-    <input type="range" min="60" max={$currentAircraft.maxSpeed - 20} step="5" class="range range-primary" bind:value={$currentAircraft.minSpeed} />
+  <h3>Aircraft Editor</h3>
+  
+  <form>
+    <label>
+      Callsign:
+      <input type="text" class="input-secondary input w-full max-w-xs" bind:value={$currentAircraft.callsign}>
     </label>
-</form>
+    
+    <label>
+      Max Altitude:
+      {formatAltitude($currentAircraft.maxAltitude)}
+      <input type="range" min={$currentAircraft.minAltitude + 1000} max="15000" step="500" class="range range-accent" bind:value={$currentAircraft.maxAltitude} />
+    </label>
+    
+    <label>
+      Min Altitude:
+      {formatAltitude($currentAircraft.minAltitude)}
+      <input type="range" min="25000" max={$currentAircraft.maxAltitude - 1000} step="100" class="range range-primary" bind:value={$currentAircraft.minAltitude} />
+    </label>
+    
+    <label>
+      Max Speed:
+      {$currentAircraft.maxSpeed}kt
+      <input type="range" min={$currentAircraft.minSpeed + 20} max="250" step="5" class="range range-accent" bind:value={$currentAircraft.maxSpeed} />
+    </label>
+    
+    <label>
+      Min Speed:
+      {$currentAircraft.minSpeed}kt
+      <input type="range" min="60" max={$currentAircraft.maxSpeed - 20} step="5" class="range range-primary" bind:value={$currentAircraft.minSpeed} />
+    </label>
+  </form>
+</div>
+  
+<style>
+  h3 {
+    margin-bottom: 1rem;
+    font-size: 1.5rem;
+  }
+  
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  label {
+    display: flex;
+    flex-direction: column;
+    gap: 0.3rem;
+  }
+
+  .container {
+    margin-top: 3rem;
+  }
+</style>
